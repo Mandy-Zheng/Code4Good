f539f8668e6d11e61665a5024541d65e2e95e15e
livlee1004 <livlee1004@gmail.com>
2023-11-28 19:43:28 -0500

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -18,7 +18,9 @@ class QuestionType(Enum):
 # make enums as necessary
 Form_Questions = {
     "name": {"type": QuestionType.TEXT, "val": ""},
-    "email": {"type": QuestionType.TEXT, "val": ""}, 
+    "email": {"type": QuestionType.TEXT, "val": ""},
+    "pref_gender": {"type": QuestionType.MULTISELECT, "val": []},
+    "travel": {"type": QuestionType.DROPDOWN, "val": -1} 
     # val could be like a list (for like multiselect) or strings or number
     # ...etc do this for other questions
 }
@@ -30,5 +32,33 @@ Form_Questions = {
 # sum up matchings of mentor/mentee form
 # add extra points for questions that are most important
 def matchingAlgorithm(mentor, mentee):
-    pass
+    # initialize compatibility score
+    score = 0
 
+    # question weights
+    dealbreaker_weight = 100
+    priority1_weight = 10
+    priority2_weight = 7
+    priority3_weight = 5
+
+    # define dealbreakers
+    dealbreakers = ["travel", ]
+
+    # iterate through form questions
+    for question, spec in Form_Questions.items():
+        question_type = spec["type"]
+        mentor_value = mentor.get(question, {}).get("val")
+        mentee_value = mentee.get(question, {}).get("val")
+
+        if question in dealbreakers:
+            if mentor_value != mentee_value:
+                total_score -= dealbreaker_weight
+            continue
+
+        if question_type == QuestionType.TEXT:
+            if mentor_value == mentee_value:
+                total_score += 1
+        elif question_type == QuestionType.MULTISELECT:
+            total += multiSelect_Equality(mentor_value, mentee_value)
+
+    return score
